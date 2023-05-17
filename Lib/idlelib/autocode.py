324987fc2f5dc7f875ea232934de4125ca00ac18
amansingh1720 <asa001@ucsd.edu>
2023-05-16 17:51:47 -0700

    dialogue box popup when user selects Code Fill

--- Lib/idlelib/autocode.py
+++ Lib/idlelib/autocode.py
@@ -6,6 +6,13 @@ See config-extension.def for configuring an extension.
 
 from idlelib.config import idleConf
 from functools import wraps
+from idlelib.config import idleConf
+from idlelib.editor import EditorWindow
+from idlelib.undo import UndoDelegator
+from idlelib.percolator import Percolator
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import simpledialog
 
 
 def format_selection(format_line):
@@ -33,6 +40,12 @@ class AutoCode:
         ] )
     ]
 
+    code_snippets = {
+        'For Loop': 'for item in sequence:\n    # Do something',
+        'While Loop': 'while condition:\n    # Do something',
+        'Try-Except': 'try:\n    # Code that might raise an exception\nexcept Exception as e:\n    # Handle the exception',
+    }
+
     def __init__(self, editwin):
         "Initialize the settings for this extension."
         self.editwin = editwin
@@ -44,14 +57,85 @@ class AutoCode:
         "Load class variables from config."
         cls.ztext = idleConf.GetOption('extensions', 'ZzDummy', 'z-text')
 
-    @format_selection
-    def z_in_event(self, line):
-        """Insert text at the beginning of each selected line.
+    # @format_selection
+    # def z_in_event(self, line):
+    #     """Insert text at the beginning of each selected line.
 
-        This is bound to the <<z-in>> virtual event when the extensions
-        are loaded.
-        """
-        return f'{self.ztext}{line}'
+    #     This is bound to the <<z-in>> virtual event when the extensions
+    #     are loaded.
+    #     """
+    #     return f'{self.ztext}{line}'
+    
+
+    # def show_code_snippets_event(self):
+    #     """Show a pop-up window with code snippets and insert the selected snippet."""
+    #     root = tk.Tk()
+    #     root.withdraw()
+    #     snippet_names = list(self.code_snippets.keys())
+    #     selected_snippet = tk.messagebox.askquestion('Code Snippets', 'Select a code snippet:', icon='question', choices=snippet_names)
+    #     if selected_snippet:
+    #         snippet_code = self.code_snippets[selected_snippet]
+    #         if isinstance(self.editwin, EditorWindow):
+    #             # If the current window is an EditorWindow, insert the snippet at the cursor position.
+    #             self.editwin.insert_into_text(snippet_code)
+    #         elif isinstance(self.editwin, tk.Text):
+    #             # If the current window is a Text widget, insert the snippet at the end of the text.
+    #             self.editwin.insert(tk.END, snippet_code)
+    #     root.destroy()
+
+
+    ## TO SHOW DIALOGUE BOX - LOOK AT grep.py 
+
+    # def code_fill_event(self):
+    #     """Show a message box with code snippets and insert the selected snippet."""
+    #     snippets = [
+    #             "import os",
+    #             "import sys",
+    #             "from tkinter import Tk, Button",
+    #             "def my_function():",
+    #             "    print('Hello, world!')"
+    #         ]
+
+    #     root = tk.Tk()
+    #     root.withdraw()  # Hide the main window
+
+    #     selected_snippet = messagebox.askquestion("Autocode Snippets",
+    #                                                 "Do you want to insert a code snippet?")
+
+
+    def code_fill_event(self, event=None):
+        snippets = [
+            "import os",
+            "import sys",
+            "from tkinter import Tk, Button",
+            "def my_function():",
+            "    print('Hello, world!')"
+        ]
+
+        root = tk.Tk()
+        root.withdraw()  # Hide the main window
+
+        selected_snippet = messagebox.askquestion("Autocode Snippets",
+                                                "Do you want to insert a code snippet?")
+
+        if selected_snippet == "yes":
+            snippet = simpledialog.askstring("Select a Snippet", "Select a code snippet:", 
+                                            parent=root, initialvalue=snippets[0])
+                                            # listboxheight=len(snippets), 
+                                            # dropdown=sorted(snippets))
+            if snippet:
+                # Insert the selected snippet at the current cursor position
+                self.insert_snippet(snippet)
+
+        root.destroy()  # Close the dialog box
+
+
+    def insert_snippet(self, snippet):
+        # Implement the logic to insert the snippet at the current cursor position
+        pass
+
+
+
 
     @format_selection
     def z_out_event(self, line):
