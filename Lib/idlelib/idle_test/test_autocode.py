657b030d8dff54ae24ff8e12de796d8ae89c63e5
amansingh1720 <asa001@ucsd.edu>
2023-05-31 13:47:15 -0700

    added functions ofr unit tests to test autocode.py

--- Lib/idlelib/idle_test/test_autocode.py
+++ Lib/idlelib/idle_test/test_autocode.py
@@ -5,6 +5,7 @@ import tkinter as tk
 from tkinter import simpledialog
 from tkinter import OptionMenu
 import unittest
+from unittest.mock import MagicMock
 from test.support import requires
 from tkinter import Tk, Text
 from unittest import mock
@@ -12,143 +13,58 @@ from idlelib import config
 from idlelib import editor
 from idlelib import format
 
-usercfg = autocode.idleConf.userCfg
-testcfg = {
-    'main': config.IdleUserConfParser(''),
-    'highlight': config.IdleUserConfParser(''),
-    'keys': config.IdleUserConfParser(''),
-    'extensions': config.IdleUserConfParser(''),
-}
-code_sample = """\
-
-class C1:
-    # Class comment.
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-"""
-
-
-class DummyEditwin:
-    get_selection_indices = editor.EditorWindow.get_selection_indices
-    def __init__(self, root, text):
-        self.root = root
-        self.top = root
-        self.text = text
-        self.fregion = format.FormatRegion(self)
-        self.text.undo_block_start = mock.Mock()
-        self.text.undo_block_stop = mock.Mock()
-
-
-class ZZDummyTest(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        requires('gui')
-        root = cls.root = Tk()
-        root.withdraw()
-        text = cls.text = Text(cls.root)
-        cls.editor = DummyEditwin(root, text)
-        zzdummy.idleConf.userCfg = testcfg
-
-    @classmethod
-    def tearDownClass(cls):
-        zzdummy.idleConf.userCfg = usercfg
-        del cls.editor, cls.text
-        cls.root.update_idletasks()
-        for id in cls.root.tk.call('after', 'info'):
-            cls.root.after_cancel(id)  # Need for EditorWindow.
-        cls.root.destroy()
-        del cls.root
-
+class TestAutoCode(unittest.TestCase):
     def setUp(self):
-        text = self.text
-        text.insert('1.0', code_sample)
-        text.undo_block_start.reset_mock()
-        text.undo_block_stop.reset_mock()
-        zz = self.zz = zzdummy.ZzDummy(self.editor)
-        zzdummy.ZzDummy.ztext = '# ignore #'
+        self.editwin = MagicMock()
+        self.text = MagicMock()
+        self.editwin.text = self.text
+        self.auto_code = autocode(self.editwin)
 
-    def tearDown(self):
-        self.text.delete('1.0', 'end')
-        del self.zz
+    def test_code_fill_event(self):
+        # Mock the Tkinter root and dialog
+        root = MagicMock()
+        dialog = MagicMock()
+        root.withdraw.return_value = None
+        dialog.result = "for i in range(10):\n    print(i)\n"
+        autocode.SnippetDialog = MagicMock(return_value=dialog)
 
-    def checklines(self, text, value):
-        # Verify that there are lines being checked.
-        end_line = int(float(text.index('end')))
+        # Call the code_fill_event method
+        self.auto_code.code_fill_event()
 
-        # Check each line for the starting text.
-        actual = []
-        for line in range(1, end_line):
-            txt = text.get(f'{line}.0', f'{line}.end')
-            actual.append(txt.startswith(value))
-        return actual
+        # Check if the snippet was inserted into the text
+        self.text.insert.assert_called_with('insert', dialog.result)
+        self.text.see.assert_called_with('insert')
 
-    def test_init(self):
-        zz = self.zz
-        self.assertEqual(zz.editwin, self.editor)
-        self.assertEqual(zz.text, self.editor.text)
+    def test_add_snippet_event(self):
+        # Mock the Tkinter root and dialog
+        root = MagicMock()
+        dialog = MagicMock()
+        code_dialog = MagicMock()
+        root.withdraw.return_value = None
+        dialog.result = "New Snippet"
+        code_dialog.text = "for i in range(n):\n    pass\n"
+        autocode.simpledialog.askstring = MagicMock(return_value=dialog.result)
+        autocode.CodeDialog = MagicMock(return_value=code_dialog)
 
-    def test_reload(self):
-        self.assertEqual(self.zz.ztext, '# ignore #')
-        testcfg['extensions'].SetOption('ZzDummy', 'z-text', 'spam')
-        zzdummy.ZzDummy.reload()
-        self.assertEqual(self.zz.ztext, 'spam')
+        # Call the add_snippet_event method
+        self.auto_code.add_snippet_event()
 
-    def test_z_in_event(self):
-        eq = self.assertEqual
-        zz = self.zz
-        text = zz.text
-        eq(self.zz.ztext, '# ignore #')
+        # Check if the new snippet was added to the snippets dictionary
+        self.assertEqual(self.auto_code.snippets[dialog.result], code_dialog.text)
 
-        # No lines have the leading text.
-        expected = [False, False, False, False, False, False, False]
-        actual = self.checklines(text, zz.ztext)
-        eq(expected, actual)
+    def test_delete_snippet_event(self):
+        # Mock the Tkinter root and dialog
+        root = MagicMock()
+        dialog = MagicMock()
+        root.withdraw.return_value = None
+        dialog.result = "For Loop"
+        autocode.SnippetDeleteDialog = MagicMock(return_value=dialog)
 
-        text.tag_add('sel', '2.0', '4.end')
-        eq(zz.z_in_event(), 'break')
-        expected = [False, True, True, True, False, False, False]
-        actual = self.checklines(text, zz.ztext)
-        eq(expected, actual)
+        # Call the delete_snippet_event method
+        self.auto_code.delete_snippet_event()
 
-        text.undo_block_start.assert_called_once()
-        text.undo_block_stop.assert_called_once()
+        # Check if the snippet was deleted from the snippets dictionary
+        self.assertNotIn(dialog.result, self.auto_code.snippets)
 
-    def test_z_out_event(self):
-        eq = self.assertEqual
-        zz = self.zz
-        text = zz.text
-        eq(self.zz.ztext, '# ignore #')
-
-        # Prepend text.
-        text.tag_add('sel', '2.0', '5.end')
-        zz.z_in_event()
-        text.undo_block_start.reset_mock()
-        text.undo_block_stop.reset_mock()
-
-        # Select a few lines to remove text.
-        text.tag_remove('sel', '1.0', 'end')
-        text.tag_add('sel', '3.0', '4.end')
-        eq(zz.z_out_event(), 'break')
-        expected = [False, True, False, False, True, False, False]
-        actual = self.checklines(text, zz.ztext)
-        eq(expected, actual)
-
-        text.undo_block_start.assert_called_once()
-        text.undo_block_stop.assert_called_once()
-
-    def test_roundtrip(self):
-        # Insert and remove to all code should give back original text.
-        zz = self.zz
-        text = zz.text
-
-        text.tag_add('sel', '1.0', 'end-1c')
-        zz.z_in_event()
-        zz.z_out_event()
-
-        self.assertEqual(text.get('1.0', 'end-1c'), code_sample)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(verbosity=2)
