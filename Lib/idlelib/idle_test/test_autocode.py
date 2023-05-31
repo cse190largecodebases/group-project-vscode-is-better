6f8cac620855f9f7fbadbd0262cd7b0a1f10405d
amansingh1720 <asa001@ucsd.edu>
2023-05-31 14:18:29 -0700

    - unit tests run - fail and do not function correctly

--- Lib/idlelib/idle_test/test_autocode.py
+++ Lib/idlelib/idle_test/test_autocode.py
@@ -1,70 +1,57 @@
-"Test zzdummy, coverage 100%."
-
-from idlelib import autocode
-import tkinter as tk
-from tkinter import simpledialog
-from tkinter import OptionMenu
 import unittest
-from unittest.mock import MagicMock
-from test.support import requires
-from tkinter import Tk, Text
-from unittest import mock
-from idlelib import config
-from idlelib import editor
-from idlelib import format
+from unittest import TestCase, mock
+from tkinter import Tk
+from tkinter import simpledialog
+from idlelib import autocode
 
-class TestAutoCode(unittest.TestCase):
+
+
+from tkinter import Tk
+
+# Create a root window to satisfy the Tkinter requirements
+root = Tk()
+root.withdraw()
+
+
+class TestAutoCode(TestCase):
+
     def setUp(self):
-        self.editwin = MagicMock()
-        self.text = MagicMock()
+        self.editwin = mock.Mock()
+        self.text = mock.Mock()
         self.editwin.text = self.text
-        self.auto_code = autocode(self.editwin)
-
-    def test_code_fill_event(self):
-        # Mock the Tkinter root and dialog
-        root = MagicMock()
-        dialog = MagicMock()
-        root.withdraw.return_value = None
-        dialog.result = "for i in range(10):\n    print(i)\n"
-        autocode.SnippetDialog = MagicMock(return_value=dialog)
-
-        # Call the code_fill_event method
-        self.auto_code.code_fill_event()
-
-        # Check if the snippet was inserted into the text
-        self.text.insert.assert_called_with('insert', dialog.result)
-        self.text.see.assert_called_with('insert')
+        self.auto_code = autocode.AutoCode(self.editwin)
 
     def test_add_snippet_event(self):
-        # Mock the Tkinter root and dialog
-        root = MagicMock()
-        dialog = MagicMock()
-        code_dialog = MagicMock()
-        root.withdraw.return_value = None
-        dialog.result = "New Snippet"
-        code_dialog.text = "for i in range(n):\n    pass\n"
-        autocode.simpledialog.askstring = MagicMock(return_value=dialog.result)
-        autocode.CodeDialog = MagicMock(return_value=code_dialog)
+        # Create a mock instance of SnippetDialog
+        with mock.patch.object(simpledialog, 'Dialog', return_value=autocode.SnippetDialog(None, None, self.auto_code.snippets)):
+            self.auto_code.add_snippet_event()
+        
+        # Verify that the snippet was added to self.text
+        self.text.insert.assert_called_once()
 
-        # Call the add_snippet_event method
-        self.auto_code.add_snippet_event()
+        # Reset the mock
+        self.text.insert.reset_mock()
 
-        # Check if the new snippet was added to the snippets dictionary
-        self.assertEqual(self.auto_code.snippets[dialog.result], code_dialog.text)
+    def test_code_fill_event(self):
+        # Create a mock instance of SnippetDialog
+        with mock.patch.object(simpledialog, 'Dialog', return_value=autocode.SnippetDialog(None, None, self.auto_code.snippets)):
+            self.auto_code.code_fill_event()
+
+        # Verify that the snippet was inserted into self.text
+        self.text.insert.assert_called_once()
+
+        # Reset the mock
+        self.text.insert.reset_mock()
 
     def test_delete_snippet_event(self):
-        # Mock the Tkinter root and dialog
-        root = MagicMock()
-        dialog = MagicMock()
-        root.withdraw.return_value = None
-        dialog.result = "For Loop"
-        autocode.SnippetDeleteDialog = MagicMock(return_value=dialog)
+        # Create a mock instance of SnippetDeleteDialog
+        with mock.patch.object(simpledialog, 'Dialog', return_value=autocode.SnippetDeleteDialog(None, None, self.auto_code.snippets)):
+            self.auto_code.delete_snippet_event()
 
-        # Call the delete_snippet_event method
-        self.auto_code.delete_snippet_event()
+        # Verify that the snippet was deleted from self.auto_code.snippets
+        self.assertEqual(len(self.auto_code.snippets), 5)  # Expected number of snippets after deletion
 
-        # Check if the snippet was deleted from the snippets dictionary
-        self.assertNotIn(dialog.result, self.auto_code.snippets)
+    # Add more test methods as needed
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main(verbosity=2)
